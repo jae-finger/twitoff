b3bfb290bf738ec609e906a80a81730d0e55744e
Jon Finger <jaefinger@gmail.com>
2020-04-22 22:29:59 -0500

    Added jinja tweets page (basic)

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,6 +1,6 @@
 # web_app/routes/twitter_routes.py
 
-from flask import Blueprint, jsonify # , request, render_template #, flash, redirect
+from flask import Blueprint, jsonify, render_template #, request, flash, redirect
 
 twitter_routes = Blueprint("twitter_routes", __name__)
 
@@ -12,3 +12,12 @@ def list_tweets():
         {"id": 3, "tweet_text": "Tweet 3"},
     ]
     return jsonify(tweets)
+
+@twitter_routes.route("/tweets")
+def list_tweets_for_humans():
+    tweets = [
+        {"id": 1, "tweet_text": "Tweet 1"},
+        {"id": 2, "tweet_text": "Tweet 2"},
+        {"id": 3, "tweet_text": "Tweet 3"},
+    ]
+    return render_template("tweets.html", message="Here are some tweets for ya'.", tweets=tweets)
