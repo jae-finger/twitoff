b725814bd0f27d605f12f450d662eccc6e833fc1
Jon Finger <jaefinger@gmail.com>
2020-04-30 23:06:09 -0500

    fixed typo in basilica service

--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -9,10 +9,10 @@ BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
 connection = basilica.Connection(BASILICA_API_KEY)
 # print(type(connection)) #> <class 'basilica.Connection'>
 
-embedding = connection.embed_sentence("hey this is a cool tweet", model="twitter")
 # print(embedding)
 # a list of 768 numbers?
 if __name__ == "__main__":
+    embedding = connection.embed_sentence("hey this is a cool tweet", model="twitter")
     tweets = ["Hello world", "artificial intelligence", "another tweet"]
     embeddings = connection.embed_sentences(tweets, model="twitter")
     for embed in embeddings:
